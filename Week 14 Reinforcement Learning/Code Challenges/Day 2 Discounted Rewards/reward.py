63e80832a4067f9f67949912bc91d32ea0afe58d
Ray Heberer <ray.heberer@gmail.com>
2018-07-10 09:29:08 -0700

    Use while loop, update gitignore

--- Week 14 Reinforcement Learning/Code Challenges/Day 2 Discounted Rewards/reward.py
+++ Week 14 Reinforcement Learning/Code Challenges/Day 2 Discounted Rewards/reward.py
@@ -2,7 +2,8 @@ def reward(R, gamma):
     total_R = 0
     reward = R
     
-    for _ in range(1000):
+    epsilon = 0.00001
+    while abs(reward) > epsilon:
         total_R += reward
         reward *= gamma
 
